e9e2cbd02a6716c28f0717c4fd6c0414a00ed06d
Udaka-Hayerson <mikehayerson@gmail.com>
2023-05-05 00:04:44 +0300

    fourteenth lesson blank

---
+++ src/main/java/java_professional/lesson_notes/fourteenth_lesson_blank/FourteenthLessonBlank.java
@@ -0,0 +1,7 @@
+package java_professional.lesson_notes.fourteenth_lesson_blank;
+
+public class FourteenthLessonBlank {
+    public static void main(String[] args) {
+        System.out.println("System.out.println();");
+    }
+}
